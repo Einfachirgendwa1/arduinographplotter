7f34e87b46831e17e19ae343a3af73c7190f575f
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2024-12-19 18:41:35 +0100

    joah

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::time::Duration;
+
+struct PointInTime {
+    x: Duration,
+    y: u32
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut points = Vec::new();
+
+    loop {
+        points.push(read_point());
+        graph_something(&points);
+    }
+}
+
+fn read_point() -> PointInTime {
+    todo!()
+}
+
+fn graph_something(points: &[PointInTime]) {
+    todo!()
 }
