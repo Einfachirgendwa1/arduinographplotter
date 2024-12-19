69baa79c8c6c27bbd7a93fc1988e66a4d7842de9
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2024-12-19 19:10:18 +0100

    ein fenster

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,53 @@
-use std::time::Duration;
+use std::{
+    cell::OnceCell,
+    sync::{Arc, Mutex},
+    thread,
+    time::{Duration, Instant},
+};
+
+use nannou::{prelude::*, App, Frame};
+
+static START: Mutex<OnceCell<Instant>> = Mutex::new(OnceCell::new());
 
 struct PointInTime {
     x: Duration,
-    y: u32
+    y: u32,
 }
 
-fn main() {
-    let mut points = Vec::new();
-
-    loop {
-        points.push(read_point());
-        graph_something(&points);
+impl PointInTime {
+    fn new(value: u32) -> Self {
+        Self {
+            x: Instant::now().duration_since(*START.lock().unwrap().get_or_init(Instant::now)),
+            y: value,
+        }
     }
 }
 
-fn read_point() -> PointInTime {
-    todo!()
+fn main() {
+    nannou::app(model).run();
 }
 
-fn graph_something(points: &[PointInTime]) {
-    todo!()
+fn model(app: &App) -> Arc<Mutex<Vec<PointInTime>>> {
+    app.new_window()
+        .title("Arduino Messwerte")
+        .view(view)
+        .build()
+        .unwrap();
+
+    let model = Arc::new(Mutex::new(Vec::new()));
+    let clone = model.clone();
+
+    thread::spawn(move || loop {
+        clone.lock().unwrap().push(PointInTime::new(read_value()));
+    });
+
+    model
+}
+
+fn view(app: &App, data: &Arc<Mutex<Vec<PointInTime>>>, frame: Frame) {
+    frame.clear(BLACK);
+}
+
+fn read_value() -> u32 {
+    0
 }
